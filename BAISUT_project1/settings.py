a1a25b29a7f259cd075ec91adf55120648bee2a2
Forhad <forhad.uddin892@gmail.com>
2024-10-29 10:41:10 +0600

    added app_test

--- BAISUT_project1/settings.py
+++ BAISUT_project1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#=z5gaxv&_3bz^c%^q2hz&mb9$7t%-myh*^_yt$q5ozjksj#5_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_test',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'BAISUT_project1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- app_test/urls.py
+++ app_test/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from .views import BookListView, BookDetailView
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book_list'),               # URL for listing all books
+    path('books1/', BookListView.as_view(), name='book_list'),               # URL for listing all books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),  # URL for book details
 ]
 
 from .views import book_list, book_detail
 
 urlpatterns = [
-    path('books/', book_list, name='book_list'),                # URL for listing all books
+    path('books2/', book_list, name='book_list'),                # URL for listing all books
     path('books/<int:pk>/', book_detail, name='book_detail'),   # URL for book details
 ]
